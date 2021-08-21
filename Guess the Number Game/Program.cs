9c2fb54bc1a711834541e1731d1b13691bc1b1a8
danikay tech <danikay tech@LAPTOP-3J25IRPG>
2021-08-21 16:39:02 +0100

    Add project files.

---
+++ Guess the Number Game/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Guess_the_Number_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int secretNumber = random.Next(1,100);
+            int userGuess = 0;
+            int userGuessCount = 0;
+            while(userGuess != secretNumber)
+            {
+                Console.Write("Guess the secret number from 1 to 100 : ");
+                userGuess = int.Parse(Console.ReadLine());
+                userGuessCount++;
+                if (userGuess < secretNumber)
+                {
+                    Console.WriteLine("Number is low!");
+                }
+                else if (userGuess > secretNumber)
+                {
+                    Console.WriteLine("Number is high!");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            // To applaud the user after guessing correctly
+            if (userGuessCount <= 2)
+            {
+                Console.WriteLine("Excellent!");
+            }
+            else if (userGuessCount <= 5)
+            {
+                if (userGuessCount > 2)
+                {
+                    Console.WriteLine("Good!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fair!");
+            }
+            Console.WriteLine("Congratulations you got the secret number right in {0} tries.", userGuessCount);
+            Console.ReadLine();
+        }
+    }
+}
